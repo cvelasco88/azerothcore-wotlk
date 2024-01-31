982471f26c585aa0a2008ecc6d2b957725cd2d38
cvelasco88 <carlosvelasco69@hotmail.com>
2024-01-31 21:07:20 +0100

    created form views for spellDbc

--- apps/web-app/controllers/SpellDbcController.php
+++ apps/web-app/controllers/SpellDbcController.php
@@ -1,12 +1,57 @@
 <?php
 
 namespace app\controllers;
+use app\models\SpellDbc;
+use \Yii;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
 
 class SpellDbcController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $dataProvider = new ActiveDataProvider([
+            'query' => SpellDbc::find(),
+            'pagination' => [
+                'pageSize' => 10,
+            ],
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
+    public function actionView($id)
+    {
+        $model = SpellDbc::findOne($id);
+
+        if ($model === null) {
+            // Spell not found
+            throw new NotFoundHttpException('The requested spell does not exist.');
+        }
+
+        return $this->render('view', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = SpellDbc::findOne($id);
+
+        if ($model === null) {
+            // Spell not found
+            throw new NotFoundHttpException('The requested spell does not exist.');
+        }
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            // Successfully updated, redirect to view action
+            return $this->redirect(['view', 'id' => $model->ID]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
 }
--- apps/web-app/views/spell-dbc/index.php
+++ apps/web-app/views/spell-dbc/index.php
@@ -1,9 +1,45 @@
 <?php
 /** @var yii\web\View $this */
-?>
-<h1>spell-dbc/index</h1>
+use app\models\SpellDbc;
+use yii\grid\GridView;
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+$this->title = 'Index Page';
+$this->params['breadcrumbs'][] = $this->title;
+
+echo GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => array_merge(
+        [
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{view} {update}',
+                /*'buttons' => [
+                    'view' => function ($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['view', 'id' => $model->ID], [
+                            'title' => Yii::t('yii', 'View'),
+                        ]);
+                    },
+                    'update' => function ($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', ['update', 'id' => $model->ID], [
+                            'title' => Yii::t('yii', 'Update'),
+                        ]);
+                    }
+                ]*/
+            ]
+        ],
+        // ['class' => 'yii\grid\SerialColumn'],
+        // array_keys((new SpellDbc())->attributes),
+        [
+            'ID',
+            'Category',
+            'Attributes',
+            'Spell Level',
+        ]
+        // Add more columns as needed
+        // ['class' => 'yii\grid\ActionColumn'],
+    ),
+]);
+
+?>
