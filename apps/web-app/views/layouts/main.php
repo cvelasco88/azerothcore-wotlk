23fcffaca63bffe6c235b064f1c574584ce0fda1
cvelasco88 <carlosvelasco69@hotmail.com>
2024-02-24 19:01:01 +0100

    removed unused file + added copyright

--- apps/web-app/views/layouts/main.php
+++ apps/web-app/views/layouts/main.php
@@ -72,7 +72,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <footer id="footer" class="mt-auto py-3 bg-light">
     <div class="container">
         <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; Hollowsaibot <?= date('Y') ?></div>
+            <div class="col-md-6 text-center text-md-start">&copy; Carlos Velasco <?= date('Y') ?></div>
             <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
         </div>
     </div>
